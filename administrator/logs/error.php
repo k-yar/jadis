a87a997da6bf9282ec26aab5dcea0e7acc8c3527
Kays Yar <kyaball@gmail.com>
2016-12-18 11:56:09 -0100

    Commit d'initialisation

---
+++ administrator/logs/error.php
@@ -0,0 +1,17 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-10-15 15:14:12 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-10-15T15:14:12+00:00	INFO ::1	joomlafailure	Cet utilisateur n'existe pas
+2016-10-15T15:14:15+00:00	INFO ::1	joomlafailure	Cet utilisateur n'existe pas
+2016-10-15T15:15:09+00:00	INFO ::1	joomlafailure	Le nom d'utilisateur ne correspond pas au mot de passe, ou vous n'avez pas encore de compte.
+2016-10-15T20:34:31+00:00	INFO ::1	joomlafailure	Cet utilisateur n'existe pas
+2016-10-16T17:02:26+00:00	INFO ::1	joomlafailure	Cet utilisateur n'existe pas
+2016-10-19T15:16:00+00:00	INFO ::1	joomlafailure	Cet utilisateur n'existe pas
+2016-10-19T15:16:08+00:00	INFO ::1	joomlafailure	Mot de passe invalide
+2016-10-23T15:58:08+00:00	INFO ::1	joomlafailure	Cet utilisateur n'existe pas
+2016-11-12T12:35:51+00:00	INFO ::1	joomlafailure	Cet utilisateur n'existe pas
+2016-11-12T12:36:38+00:00	INFO ::1	joomlafailure	Le nom d'utilisateur ne correspond pas au mot de passe, ou vous n'avez pas encore de compte.
+2016-11-24T21:35:06+00:00	INFO ::1	joomlafailure	Le nom d'utilisateur ne correspond pas au mot de passe, ou vous n'avez pas encore de compte.
